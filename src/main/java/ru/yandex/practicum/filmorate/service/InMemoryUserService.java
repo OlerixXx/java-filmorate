6995d949c1e38f055e68449f3c21d61c01dee32d
OlerixXx <olegzka.mprozov@yandex.ru>
2023-12-19 03:49:53 +0300

    ТЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/service/InMemoryUserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/InMemoryUserService.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
